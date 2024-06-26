a755731c273ad66c6c960c4f51f08eeaa053ca05
MAHASHANA <raghuhanuma@gmail.com>
2024-06-26 00:43:36 -0400

    YAMSPy based serial communication for FC

---
+++ IMU_and_opticflow.py
@@ -0,0 +1,97 @@
+import sys
+import time
+import serial
+import struct
+from yamspy import MSPy
+
+# Define the serial port
+serial_port = "/dev/ttyACM0"
+
+class CustomMSPy(MSPy):
+    def fast_read_imu(self):
+        """Request, read and process RAW IMU"""
+
+        # Request IMU values
+        if self.send_RAW_msg(MSPy.MSPCodes['MSP_RAW_IMU']):
+            # $ + M + < + data_length + msg_code + data + msg_crc
+            # 6 bytes + data_length
+            # data_length: 9 x 2 = 18 bytes
+            data_length = 18
+            msg = self.receive_raw_msg(size=(6 + data_length))[5:]
+            converted_msg = struct.unpack('<%dh' % (data_length / 2), msg[:-1])
+
+            # /512 for mpu6050, /256 for mma
+            # currently we are unable to differentiate between the sensor types, so we are going with 512
+            # And what about SENSOR_CONFIG???
+            self.SENSOR_DATA['accelerometer'][0] = converted_msg[0]
+            self.SENSOR_DATA['accelerometer'][1] = converted_msg[1]
+            self.SENSOR_DATA['accelerometer'][2] = converted_msg[2]
+
+            # properly scaled (INAV and BF use the same * (4 / 16.4))
+            # but this is supposed to be RAW, so raw it is!
+            self.SENSOR_DATA['gyroscope'][0] = converted_msg[3]
+            self.SENSOR_DATA['gyroscope'][1] = converted_msg[4]
+            self.SENSOR_DATA['gyroscope'][2] = converted_msg[5]
+
+            # no clue about scaling factor (/1090), so raw
+            self.SENSOR_DATA['magnetometer'][0] = converted_msg[6]
+            self.SENSOR_DATA['magnetometer'][1] = converted_msg[7]
+            self.SENSOR_DATA['magnetometer'][2] = converted_msg[8]
+
+            return self.SENSOR_DATA
+        else:
+            print("Failed to send IMU read command")
+            return None
+
+    def fast_read_optical_flow(self):
+        """Request, read and process optical flow data"""
+
+        if self.send_RAW_msg(MSPy.MSPCodes['MSPV2_INAV_OPTICAL_FLOW']):
+            # Assume a data length of 10 bytes for optical flow
+            data_length = 10
+            msg = self.receive_raw_msg(size=(6 + data_length))[5:]
+            converted_msg = struct.unpack('<%dh' % (data_length / 2), msg[:-1])
+
+            optical_flow_data = {
+                'flowX': converted_msg[0],
+                'flowY': converted_msg[1],
+                'flowCompX': converted_msg[2],
+                'flowCompY': converted_msg[3],
+                'quality': converted_msg[4]
+            }
+
+            return optical_flow_data
+        else:
+            print("Failed to send optical flow read command")
+            return None
+
+def read_imu_data(board):
+    """Read IMU data from the flight controller"""
+    imu_data = board.fast_read_imu()
+    if imu_data:
+        print(f"IMU Data: {imu_data}")
+    else:
+        print("Failed to read IMU data")
+
+def read_optical_flow_data(board):
+    """Read optical flow data from the flight controller"""
+    optical_flow_data = board.fast_read_optical_flow()
+    if optical_flow_data:
+        print(f"Optical Flow Data: {optical_flow_data}")
+    else:
+        print("Failed to read optical flow data")
+
+# Initialize MSPy with detailed logging
+with CustomMSPy(device=serial_port, logfilename='MSPy.log', logfilemode='a', loglevel='DEBUG') as board:
+    if board == 1:
+        print("An error occurred... probably the serial port is not available ;)")
+        sys.exit(1)
+
+    # Read IMU data
+    read_imu_data(board)
+    time.sleep(1)
+
+    # Read optical flow data
+    read_optical_flow_data(board)
+    time.sleep(1)
+
---
+++ Motorcontroll.py
@@ -0,0 +1,66 @@
+import sys
+import time
+import serial
+from yamspy import MSPy
+
+# Define the serial port
+serial_port = "/dev/ttyACM0"
+
+# Avoid dangerous commands
+avoid_list = ['MSP_DATAFLASH_ERASE', 'MSP_DATAFLASH_READ', 'MSP_EEPROM_WRITE']
+
+# Initialize MSPy with detailed logging
+with MSPy(device=serial_port, logfilename='MSPy.log', logfilemode='a', loglevel='DEBUG') as board:
+    if board == 1:
+        print("An error occurred, probably the serial port is not available")
+        sys.exit(1)
+
+    def arm_drone():
+        """Arm the drone using RC channels"""
+        rc_data = [1500, 1500, 1500, 1000, 2000, 1000, 1000, 1000]  # Example RC data to arm
+        if board.send_RAW_RC(rc_data):
+            print("Arming command sent")
+        else:
+            print("Failed to send arming command")
+
+    def disarm_drone():
+        """Disarm the drone using RC channels"""
+        rc_data = [1500, 1500, 1500, 1000, 1000, 1000, 1000, 1000]  # Example RC data to disarm
+        if board.send_RAW_RC(rc_data):
+            print("Disarming command sent")
+        else:
+            print("Failed to send disarming command")
+
+    def set_motor_speeds(motor_speeds):
+        """Set the speeds for the motors"""
+        if len(motor_speeds) != 4:
+            raise ValueError("Motor speeds should be a list of four values")
+        # Send motor speeds (pad with zeros for remaining motors)
+        if board.send_RAW_MOTORS(motor_speeds + [0, 0, 0, 0]):
+            print(f"Motor speeds set to: {motor_speeds}")
+        else:
+            print("Failed to set motor speeds")
+
+    # Arm the drone
+    arm_drone()
+    time.sleep(2)  # Wait for the command to be processed
+
+    # Set motor speeds (values between 1000 and 2000)
+    motor_speeds = [1500, 1500, 1500, 1500]  # Example motor speeds
+    set_motor_speeds(motor_speeds)
+    time.sleep(3)  # Keep motors running for 5 seconds
+
+    # Disarm the drone
+    disarm_drone()
+    time.sleep(2)  # Wait for the command to be processed
+
+    # Print arming disable flags
+    print("armingDisableFlags: {}".format(board.process_armingDisableFlags(board.CONFIG['armingDisableFlags'])))
+
+    # Reboot the flight controller (if needed)
+    if board.reboot():
+        try:
+            dataHandler = board.receive_msg()
+            board.process_recv_data(dataHandler)
+        except serial.SerialException:
+            print("Board is rebooting")
